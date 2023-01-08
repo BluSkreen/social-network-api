8b380e2c20239bf022800620010b8e562409ffe4
BluSkreen <jasonduran303@gmail.com>
2023-01-07 22:55:12 -0700

    controller templates

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
+
+module.exports = {
+    //    /api/thoughts
+
+    // get all thoughts
+    
+    // create a thought
+
+    //    /api/thoughts/:thoughtId
+    
+    // get single thought
+    
+    // delete a thought
+
+    // update a thought
+
+    //    /api/thoughts/:thoughtId/reactions
+
+    // add a reaction
+
+    // remove a reaction
+
+};
---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    //    /api/users
+
+    // get all users
+    
+    // create a User
+
+    //    /api/users/:userId 
+    
+    // get a user
+    
+    // delete a user
+
+    // update a user
+
+    //    /api/user/:userId/friends/:friendsId
+
+    // add a friend
+
+    // remove a friend
+
+};
