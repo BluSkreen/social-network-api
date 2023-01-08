f940af26a11677cc82aca888fc13ea07e74b0695
BluSkreen <jasonduran303@gmail.com>
2023-01-07 22:33:48 -0700

    userRoutes and thoughtRoutes are set up

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+
+const {
+    createThought,
+    getThoughts,
+    getSingleThought,
+    updateThought,
+    createReaction,
+    removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+    .route("/:thoughtId")
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(removeThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route("/thoughts/:thoughtId/reactions")
+    .post(createReaction)
+    .delete(removeReaction);
+
+module.exports = router;
